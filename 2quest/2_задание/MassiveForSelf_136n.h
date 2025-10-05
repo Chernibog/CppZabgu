d8c1761e1fa7636cbe23ff20547710c625b5c4ca
Chernibog <nikitanofamily@gmail.com>
2025-10-05 21:12:39 +0900

    Add files via upload

---
+++ 2quest/2_задание/MassiveForSelf_136n.h
@@ -0,0 +1,9 @@
+
+//заголовочный файл
+
+#ifndef CPPZABGU_MASSIVEFORSELF_136N_H
+#define CPPZABGU_MASSIVEFORSELF_136N_H
+
+std::vector<double> Massive_from_file();
+
+#endif //CPPZABGU_MASSIVEFORSELF_136N_H
