e130dd368d142c98d88e07f4df96e35dd1e99a05
Chernibog <nikitanofamily@gmail.com>
2025-10-14 14:01:50 +0900

    Update MassiveForSelf_136n.cpp

--- 2quest/2_task/MassiveForSelf_136n.cpp
+++ 2quest/2_task/MassiveForSelf_136n.cpp
@@ -14,6 +14,7 @@ vector<double> Massive_from_file(const string& filename) {
     vector<double> numbers_a;
     ifstream file(filename);
     if (!file.is_open()) {
+        //сообщение в throw
         cerr << "Ошибка: не удалось открыть файл f.txt!" << endl;
         return numbers_a; 
     }
@@ -22,16 +23,16 @@ vector<double> Massive_from_file(const string& filename) {
     while (getline(file, line)) {
         // Проверка, что строка не пустая (на случай пустых строк в файле)
         if (!line.empty()) {
-            try {
-                /*
-                 * stod вернет invalid_argument&, если например будет передано "привет" - символьный тип
-                 * stod вернет out_of_range&, если например будет передано "99999999999999999" - слишком большое значение
-                 */
-                numbers_a.push_back(stod(line));
-            } catch (const invalid_argument& e) {
-                throw invalid_argument("Предупреждение: пропущена недопустимая строка: " + line);
-            } catch (const out_of_range& e) {
-                throw  out_of_range("Предупреждение: число вне диапазона: "+ line);
+            // try { перенести в main
+            //     /*
+            //      * stod вернет invalid_argument&, если например будет передано "привет" - символьный тип
+            //      * stod вернет out_of_range&, если например будет передано "99999999999999999" - слишком большое значение
+            //      */
+            //     numbers_a.push_back(stod(line));
+            // } catch (const invalid_argument& e) {
+            //     throw invalid_argument("Предупреждение: пропущена недопустимая строка: " + line);
+            // } catch (const out_of_range& e) {
+            //     throw  out_of_range("Предупреждение: число вне диапазона: "+ line);
             }
         }
     }
@@ -39,5 +40,6 @@ vector<double> Massive_from_file(const string& filename) {
     file.close();
     return numbers_a;
     
+
 
 }
